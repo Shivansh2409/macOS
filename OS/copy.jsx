0a9f29528304e61cf515a0c8b4dce43ed6b7f877
Shivansh Rathor <shivanshrathore61@gmail.com>
2026-02-01 18:29:08 +0530

    finder completed images left

---
+++ OS/copy.jsx
@@ -0,0 +1,87 @@
+<div className="top">
+  <div className="head">
+    <h1>
+      <Folder className="f-i"></Folder>
+      HackCBS
+    </h1>
+    <p>
+      <Clock className="f-i" /> Nov 9, 2025
+      <MapPin className="f-i" /> Delhi
+    </p>
+  </div>
+  <div className="main">
+    <h4>ABOUT PROJECT</h4>
+    <p className="detail">
+      HackCBS is the annual flagship hackathon of Shri Mata Vaishno Devi
+      University, attracting tech enthusiasts from across the nation. The event
+      fosters innovation, collaboration, and problem -solving over a thrilling
+      36-hour coding marathon. Participants engage in workshops, mentorship
+      sessions, and networking opportunities, culminating in a grand showcase of
+      cutting-edge projects. HackCBS not only celebrates technological
+      creativity but also builds a vibrant community of developers,
+      entrepreneurs, and visionaries dedicated to shaping the future through
+      technology.
+    </p>
+    <h4>TECH STACK</h4>
+    <div className="tech-tag">
+      <div className="t-t">ReactJS</div>
+      <div className="t-t">NodeJS</div>
+      <div className="t-t">MongoDB</div>
+      <div className="t-t">ExpressJS</div>
+      <div className="t-t">HTML/CSS</div>
+      <div className="t-t">JavaScript</div>
+    </div>
+    <h6>
+      <ImageIcon className="i-ii" /> Project Images &nbsp; &nbsp;
+    </h6>
+    <div className="imgs-event">
+      <div className="img-box">
+        <img
+          src="https://images.unsplash.com/photo-1769650796145-30df10357926?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+          alt="Project 1"
+        />
+      </div>
+      <div className="img-box">
+        <img
+          src="https://images.unsplash.com/photo-1769650796145-30df10357926?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+          alt="Project 2"
+        />
+      </div>
+      <div className="img-box">
+        <img
+          src="https://images.unsplash.com/photo-1769650796145-30df10357926?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+          alt="Project 3"
+        />
+      </div>
+    </div>
+    <h6>
+      <Monitor className="i-id" /> Event Images &nbsp; &nbsp;
+    </h6>
+    <div className="imgs-project">
+      <div className="img-box">
+        <img
+          src="https://images.unsplash.com/photo-1769650796145-30df10357926?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+          alt="Project 1"
+        />
+      </div>
+      <div className="img-box">
+        <img
+          src="https://images.unsplash.com/photo-1769650796145-30df10357926?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+          alt="Project 1"
+        />
+      </div>
+      <div className="img-box">
+        <img
+          src="https://images.unsplash.com/photo-1769650796145-30df10357926?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+          alt="Project 1"
+        />
+      </div>
+      <div className="img-box">
+        <img
+          src="https://images.unsplash.com/photo-1769650796145-30df10357926?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+          alt="Project 1"
+        />
+      </div>
+    </div>
+  </div>
+</div>;
--- OS/src/components/Grid.jsx
+++ OS/src/components/Grid.jsx
@@ -38,93 +38,54 @@ const Grid = ({ items, setItems }) => {
   return (
     <>
       <div className="grid-container">
-        <div className="top">
-          <div className="head">
-            <h1>
-              <Folder className="f-i"></Folder>
-              HackCBS
-            </h1>
-            <p>
-              <Clock className="f-i" /> Nov 9, 2025
-              <MapPin className="f-i" /> Delhi
-            </p>
-          </div>
-          <div className="main">
-            <h4>ABOUT PROJECT</h4>
-            <p className="detail">
-              HackCBS is the annual flagship hackathon of Shri Mata Vaishno Devi
-              University, attracting tech enthusiasts from across the nation.
-              The event fosters innovation, collaboration, and problem -solving
-              over a thrilling 36-hour coding marathon. Participants engage in
-              workshops, mentorship sessions, and networking opportunities,
-              culminating in a grand showcase of cutting-edge projects. HackCBS
-              not only celebrates technological creativity but also builds a
-              vibrant community of developers, entrepreneurs, and visionaries
-              dedicated to shaping the future through technology.
-            </p>
-            <h4>TECH STACK</h4>
-            <div className="tech-tag">
-              <div className="t-t">ReactJS</div>
-              <div className="t-t">NodeJS</div>
-              <div className="t-t">MongoDB</div>
-              <div className="t-t">ExpressJS</div>
-              <div className="t-t">HTML/CSS</div>
-              <div className="t-t">JavaScript</div>
-            </div>
-            <h6>
-              <ImageIcon className="i-ii" /> Project Images &nbsp; &nbsp;
-            </h6>
-            <div className="imgs-event">
-              <div className="img-box">
-                <img
-                  src="https://images.unsplash.com/photo-1769650796145-30df10357926?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
-                  alt="Project 1"
-                />
+        {hackathonDetails.map(
+          (hackathon) =>
+            items === hackathon.name && (
+              <div className="top">
+                <div className="head">
+                  <h1>
+                    <Folder className="f-i"></Folder>
+                    {hackathon.name}
+                  </h1>
+                  <p>
+                    <Clock className="f-i" /> {hackathon.date}
+                    <MapPin className="f-i" /> {hackathon.location}
+                  </p>
+                </div>
+                <div className="main">
+                  <h4>ABOUT PROJECT</h4>
+                  <p className="detail">{hackathon.description}</p>
+                  <h4>TECH STACK</h4>
+                  <div className="tech-tag">
+                    {hackathon.techStack.map((tech) => (
+                      <div className="t-t">{tech}</div>
+                    ))}
+                  </div>
+
+                  <h6>
+                    <ImageIcon className="i-ii" /> Project Images &nbsp; &nbsp;
+                  </h6>
+                  <div className="imgs-event">
+                    {hackathon.imagesProject.map((imgSrc, index) => (
+                      <div className="img-box" key={index}>
+                        <img src={imgSrc} alt={`Project ${index + 1}`} />
+                      </div>
+                    ))}
+                  </div>
+                  <h6>
+                    <Monitor className="i-id" /> Event Images &nbsp; &nbsp;
+                  </h6>
+                  <div className="imgs-project">
+                    {hackathon.imagesEvent.map((imgSrc, index) => (
+                      <div className="img-box" key={index}>
+                        <img src={imgSrc} alt={`Event ${index + 1}`} />
+                      </div>
+                    ))}
+                  </div>
+                </div>
               </div>
-              <div className="img-box">
-                <img
-                  src="https://images.unsplash.com/photo-1769650796145-30df10357926?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
-                  alt="Project 2"
-                />
-              </div>
-              <div className="img-box">
-                <img
-                  src="https://images.unsplash.com/photo-1769650796145-30df10357926?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
-                  alt="Project 3"
-                />
-              </div>
-            </div>
-            <h6>
-              <Monitor className="i-id" /> Event Images &nbsp; &nbsp;
-            </h6>
-            <div className="imgs-project">
-              <div className="img-box">
-                <img
-                  src="https://images.unsplash.com/photo-1769650796145-30df10357926?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
-                  alt="Project 1"
-                />
-              </div>
-              <div className="img-box">
-                <img
-                  src="https://images.unsplash.com/photo-1769650796145-30df10357926?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
-                  alt="Project 1"
-                />
-              </div>
-              <div className="img-box">
-                <img
-                  src="https://images.unsplash.com/photo-1769650796145-30df10357926?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
-                  alt="Project 1"
-                />
-              </div>
-              <div className="img-box">
-                <img
-                  src="https://images.unsplash.com/photo-1769650796145-30df10357926?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
-                  alt="Project 1"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
+            ),
+        )}
         <div className="bottom ">
           <div
             className={`third grid-body ${items === "HackCBS 8.0" ? "active" : ""}`}
